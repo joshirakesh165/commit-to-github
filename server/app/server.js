bc59bd781c38142052b7b9ff95cd4a5d3dd56063
joshirakesh165 <testing@gmail.com>
2024-02-29 00:17:46 +0530

    Code pushed from Api

---
+++ server/app/server.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+const app = express();
+import formidable from 'express-formidable'
+import extractFolderAndGetData from './fs.js';
+import pushLocalChanges from './githubAPI.js';
+
+var corsOptions = {
+    origin: "*"
+}
+app.use(cors(corsOptions));
+
+const SERVICE_PORT = process.env.SERVICE_PORT || 8000
+
+app.use(bodyParser.json());
+app.use(formidable())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.post('/upload', async function (req, res) {
+    let { owner, repo, token, commitMessage, email } = req.fields;
+    let files = extractFolderAndGetData(req.files.file.path);
+    let message = await pushLocalChanges(owner, repo, token, commitMessage, email, files);
+    if (message instanceof Error) {
+        res.status(500).send({ status: 500, message: "Internal server error" });
+    } else {
+        res.status(200).send({ status: 200, message });
+    }
+});
+
+
+
+app.listen(SERVICE_PORT, () => {
+    console.log('server running on ', SERVICE_PORT)
+});
+
+
+
